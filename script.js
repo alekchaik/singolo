e83292e14641534c211e05125ea2f8f17e6722da
 Александр <patrisbane@MacBook-Pro-Aleksandr.local>
2020-03-16 12:30:20 +0300

    make animation for phones

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const header = document.querySelector('.header');
+const menu = document.querySelectorAll('.navigation li a');
+function toggleState(event) {
+    menu.forEach(el => el.removeAttribute('id'));
+    event.target.id = "navigation_selected";
+    console.log(event.target.classList[0] != 'home')
+    if (event.target.classList[0] != "home")
+        header.style.display = 'none';
+}
+window.addEventListener('mousewheel', (event) => {
+    header.style.display = 'inherit';
+})
+menu.forEach(el => el.addEventListener('click', toggleState))
+
 const slider = document.querySelector('.slider__phones');
 const arrows = document.querySelectorAll('.arrow img');
 const root = document.documentElement;
@@ -33,8 +47,14 @@ function slidePhones(el) {
         },0);
     }
     isFirstActive = !isFirstActive;
-
 }
 
 arrows[0].addEventListener('click',slidePhones);
 arrows[1].addEventListener('click', slidePhones)
+
+const phones = document.querySelectorAll('.first_slide > div div');
+function togglePhone(event) {
+    event.target.parentNode.parentNode.classList.toggle('off')
+}
+phones.forEach(el => el.addEventListener('click', togglePhone))
+// verticalPhone.addEventListener('click',()=> console.log('123'))
